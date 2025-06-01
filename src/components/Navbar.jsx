ff24347a10c6cc0160b65ff4a229a20b8d870c81
anggarchmdi <anggarchmdi@gmail.com>
2025-06-01 19:49:10 +0700

    fix navbar font-size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar() {
     <div className="fixed top-0 z-10 bg-gray-900 text-white w-full text-2xl flex justify-between items-center underline border-b">
         <div className="px-4 py-4">
         <Link to='/'>
-            <h1 className='font-semibold text-3xl '>Aplikasi Pencatatan</h1>
+            <h1 className='font-semibold lg:text-3xl '>Aplikasi Pencatatan</h1>
         </Link>
         </div>
         <div className="px-8 py-4 transition-all transform hover:scale-95 duration-300 hover:cursor-pointer">
